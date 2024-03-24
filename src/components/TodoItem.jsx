7e73189ea9b2e951dae9995a3afbd2fa280a910c
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-03-24 19:21:03 +0800

    added delete.toggle functionality

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,20 @@
-export default function TodoItem() {
-  return <div>TodoItem</div>;
+import useStore from "../useStore";
+export default function TodoItem({ todo }) {
+  const deleteTodo = useStore((state) => state.deleteTodo);
+  const toggleTodo = useStore((state) => state.toggleTodo);
+  return (
+    <li className="border border-slate-300 p-2">
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleTodo(todo.id)}
+      />
+      <span
+        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      >
+        {todo.text}
+      </span>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </li>
+  );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import useStore from "../useStore";
+import TodoItem from "./TodoItem";
 
 export default function TodoList() {
-  return <div>TodoList</div>;
+  const todos = useStore((state) => state.todos);
+  const addTodo = useStore((state) => state.addTodo);
+
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(text);
+    setText("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleChange} />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
 }
